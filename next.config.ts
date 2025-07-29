fcfabf0aba323529342f228854ece048ca039c1a
Susheel Kumar <thotasusheelkumar04@gmail.com>
2025-07-29 14:11:48 +0000

    restore the checkpoint previous

--- next.config.ts
+++ next.config.ts
@@ -34,12 +34,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'image.freepik.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
