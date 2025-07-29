5cb0174b4d717c61d8098bfedc8dd86a8ff24a49
Susheel Kumar <thotasusheelkumar04@gmail.com>
2025-07-29 13:59:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/how-it-works.tsx
+++ src/components/sections/how-it-works.tsx
@@ -23,7 +23,7 @@ export function HowItWorksSection() {
                     </div>
                      <div className="overflow-hidden rounded-lg shadow-[0_0_50px_rgba(155,135,245,0.2)]">
                         <Image
-                            src="https://images.unsplash.com/photo-1589307904488-7d60ff29c975?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxMQVclMjBJTkRJQXxlbnwwfHx8fDE3NTM3OTc0NTh8MA&ixlib=rb-4.1.0&q=80&w=1080"
+                            src="https://images.unsplash.com/photo-1496104679561-38d3af73f9b0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxM3x8bGF3JTIwYm9va3N8ZW58MHx8fHwxNzUzNzk3NTI4fDA&ixlib=rb-4.1.0&q=80&w=1080"
                             alt="Data analysis dashboard showing charts and graphs"
                             width={1920}
                             height={1080}
