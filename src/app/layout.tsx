fdedf8b36c086e2eae85749a3b05b98cb9e25055
Susheel Kumar <thotasusheelkumar04@gmail.com>
2025-07-29 15:29:18 +0000

    add a logo related to legal in the fevicon chane the current one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%232E5DA8' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpath d='m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z'/%3e%3cpath d='m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z'/%3e%3cpath d='M7 21h10'/%3e%3cpath d='M12 3v18'/%3e%3cpath d='M3 7h18'/%3e%3c/svg%3e" />
+        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%232E5DA8' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpath d='m14 14-7 7'%3e%3c/path%3e%3cpath d='m20 20-7-7'%3e%3c/path%3e%3cpath d='m3 21 7-7'%3e%3c/path%3e%3cpath d='m16 8 5 5'%3e%3c/path%3e%3cpath d='M7 10h1.1c.3 0 .6.2.8.4l1.6 1.6c.2.2.5.4.8.4H12c.3 0 .6.2.8.4l1.6 1.6c.2.2.5.4.8.4H17c.3 0 .6.2.8.4l1.1 1.1'%3e%3c/path%3e%3cpath d='m4.5 12.5 5 5'%3e%3c/path%3e%3cpath d='m16 8 5 5'%3e%3c/path%3e%3c/svg%3e" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
